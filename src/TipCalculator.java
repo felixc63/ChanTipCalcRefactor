97bb4b903cd3306e100671cef7e530b018d7cfa6
Felix Chan <felixc63@nycstudents.net>
2022-10-07 12:02:01 -0400

    Finished

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, double tipPercentage, double totalBillBeforeTip){
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage/100;
+        this.totalBillBeforeTip = totalBillBeforeTip;
+    }
+
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
+
+    public double getTipPercentage(){
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost){
+        cost += totalBillBeforeTip;
+    }
+
+    public double tipAmount(){
+        return tipPercentage*totalBillBeforeTip;
+    }
+
+    public double totalBill(){
+        return tipAmount()+totalBillBeforeTip;
+    }
+
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip/numPeople;
+    }
+
+    public double perPersonTipAmount(){
+        return tipAmount()/numPeople;
+    }
+
+    public double perPersonTotalCost(){
+        return totalBill()/numPeople;
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+
+public class TipCalculatorRunner {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Your total bill: ");
+        double totalBill = scan.nextDouble();
+
+        System.out.print("Tip percentage: ");
+        double tipPercentage = scan.nextInt();
+
+        System.out.print("Number of people in the party: ");
+        int numberOfPeople = scan.nextInt();
+
+        TipCalculator calc = new TipCalculator(numberOfPeople, tipPercentage, totalBill);
+
+        System.out.println("Your total tip amount is $" + String.format("%.2f",calc.tipAmount()));
+        System.out.println("Your total bill including tip is $" + String.format("%.2f", calc.totalBill()));
+        System.out.println("Each person pays $" + String.format("%.2f", calc.perPersonTipAmount()) + " in tips");
+        System.out.println("Each person pays $" + String.format("%.2f", calc.perPersonTotalCost()) + " in total including bill and tip");
+    }
 }
